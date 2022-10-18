c84849cb472d0fa8e4f7ea6538dc3c697c0d7c13
Tosin Ezekiel <tosinezekiel8@gmail.com>
2022-10-18 20:37:19 +0100

    Update foody_createaccount.dart

--- lib/screen/foody_createaccount.dart
+++ lib/screen/foody_createaccount.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+import '../widgets/constants.dart';
 
 class CreateAccount extends StatelessWidget {
   const CreateAccount({super.key});
@@ -22,6 +25,29 @@ class CreateAccount extends StatelessWidget {
                         fit: BoxFit.cover,
                         colorFilter: ColorFilter.mode(
                             Colors.black54, BlendMode.darken))))),
+        Column(children: [
+          SizedBox(height: size.width * 0.1),
+          Stack(children: [
+            Center(
+              child: CircleAvatar(
+                  radius: size.width * 0.14,
+                  backgroundColor: Colors.grey.withOpacity(0.5),
+                  child: Icon(
+                    FontAwesomeIcons.user,
+                    color: kWhite,
+                    size: size.width * 0.1,
+                  )),
+            ),
+            Container(
+              width: size.width * 0.12,
+              height: size.width * 0.12,
+              decoration: BoxDecoration(
+                  border: Border.all(color: kWhite, width: 1),
+                  shape: BoxShape.circle),
+            ),
+          ]),
+          SizedBox(height: size.width * 0.1),
+        ]),
       ],
     );
   }
