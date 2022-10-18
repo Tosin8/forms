b11729fc21b6222c9c83e555122f0a5fd7dd00c6
Tosin Ezekiel <tosinezekiel8@gmail.com>
2022-10-18 20:15:22 +0100

    Update foody_createaccount.dart

--- lib/screen/foody_createaccount.dart
+++ lib/screen/foody_createaccount.dart
@@ -5,6 +5,24 @@ class CreateAccount extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    Size size = MediaQuery.of(context).size;
+    return Stack(
+      children: [
+        ShaderMask(
+            shaderCallback: (rect) => const LinearGradient(
+                  begin: Alignment.bottomCenter,
+                  end: Alignment.center,
+                  colors: [Colors.black, Colors.transparent],
+                ).createShader(rect),
+            blendMode: BlendMode.darken,
+            child: Container(
+                decoration: const BoxDecoration(
+                    image: DecorationImage(
+                        image: AssetImage('assets/images/register.jpg'),
+                        fit: BoxFit.cover,
+                        colorFilter: ColorFilter.mode(
+                            Colors.black54, BlendMode.darken))))),
+      ],
+    );
   }
 }
