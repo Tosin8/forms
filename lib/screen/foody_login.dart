591722b0916a6b06e73e78bce2771edf55b8d7b9
Tosin Ezekiel <tosinezekiel8@gmail.com>
2022-10-18 15:51:21 +0100

    Update foody_login.dart

--- lib/screen/foody_login.dart
+++ lib/screen/foody_login.dart
@@ -11,12 +11,20 @@ class _foody_loginState extends State<foody_login> {
   @override
   Widget build(BuildContext context) {
     return Stack(children: [
-      Container(
-        decoration: const BoxDecoration(
-          image: DecorationImage(
-            image: AssetImage('assets/images/login.jpg'),
-            fit: BoxFit.cover,
-            colorFilter: ColorFilter.mode(Colors.black54, BlendMode.darken),
+      ShaderMask(
+        shaderCallback: (rect) => const LinearGradient(
+          begin: Alignment.bottomCenter,
+          end: Alignment.center,
+          colors: [Colors.black, Colors.transparent],
+        ).createShader(rect),
+        blendMode: BlendMode.darken,
+        child: Container(
+          decoration: const BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage('assets/images/login.jpg'),
+              fit: BoxFit.cover,
+              colorFilter: ColorFilter.mode(Colors.black54, BlendMode.darken),
+            ),
           ),
         ),
       )
