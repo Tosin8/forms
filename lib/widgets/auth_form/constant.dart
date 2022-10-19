8197fcd8c3f1875b97b25576bd9d742e6617df7f
Tosin Ezekiel <tosinezekiel8@gmail.com>
2022-10-19 20:15:49 +0100

    Create constant.dart

---
+++ lib/widgets/auth_form/constant.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+const Color darkBlue = Color(0xff092E34);
+const Color lightBlue = Color(0xff489FB5);
+const Color orange = Color(0xffFFA62B);
+const Color darkOrange = Color(0xffCC7700);
